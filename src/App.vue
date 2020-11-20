61c0a1e2c168912ce891a373f08030b21a1ea9a5
Facundo <facundopossee@gmail.com>
2020-11-20 11:59:42 -0300

    Fixes

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,86 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div>
+        <h3>Fractions validator</h3>
+        <input type="text" placeholder="enter a fraction" v-model="fraction" />
+        <p>{{ this.fractionresult }}</p>
+      </div>
+      <br />
+      <div>
+        <h3>Names validator</h3>
+        <input type="text" placeholder="enter a name" v-model="name" />
+        <p>{{ this.nameresult }}</p>
+      </div>
+    </div>
+    <div class="text">
+      <h5>
+        This website was developed for an E3 Ecommerce technical exam by
+        <a href="https://facundoposse.vercel.app/">Facundo Posse</a>
+      </h5>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app",
+  data() {
+    return {
+      name: "",
+      fraction: "",
+      nameresult: false,
+      fractionresult: "",
+    };
+  },
+  methods: {
+    gcd(a, b) {
+      var aux;
+      var i1 = Math.max(a, b);
+      var i2 = Math.min(a, b);
+      do {
+        aux = i2;
+        i2 = i1 % i2;
+        i1 = aux;
+      } while (i2 !== 0);
+      return i1;
+    },
+  },
+  watch: {
+    fraction: function () {
+      let a = /^[0-9]{1,}\/[0-9]{1,}$/.test(this.fraction);
+      if (a == false) {
+        this.fractionresult = "not a fraction";
+      } else {
+        let arr = this.fraction.split("/", 2);
+        let div = this.gcd(arr[0], arr[1]);
+
+        this.fractionresult = arr[0] / div + "/" + arr[1] / div;
+      }
+    },
+    name: function () {
+      let a = /^([A-Z][a-z]{2,}( [A-Z][a-z]{2,}( [A-Z][a-z]{2,})?| [A-Z]\.( [A-Z][a-z]{2,})?)|([A-Z]\.( [A-Z][a-z]{2,}))|[A-Z]\.( [A-Z]\.)( [A-Z][a-z]{2,}))$/.test(
+        this.name
+      );
+      this.nameresult = a;
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 30vh;
+}
+.container {
+  margin: 0 auto;
+}
+.text {
+  padding-top: 20px;
 }
 </style>
