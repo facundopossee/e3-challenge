63db37ddd498e43755acd6e85cb80a4f826b5838
Facundo <facundopossee@gmail.com>
2020-11-26 11:11:17 -0300

    add gcd recursive method and comments

--- src/App.vue
+++ src/App.vue
@@ -38,34 +38,35 @@ export default {
     };
   },
   methods: {
+    //METODO QUE CALCULA RECURSIVAMENTE EL MAXIMO COMUN DIVISOR, AL RECIBIR DOS VALORES    
     gcd(a, b) {
-      var aux;
-      var i1 = Math.max(a, b);
-      var i2 = Math.min(a, b);
-      do {
-        aux = i2;
-        i2 = i1 % i2;
-        i1 = aux;
-      } while (i2 !== 0);
-      return i1;
+      if (b == 0) return a;
+      else return this.gcd(b, a % b);
     },
   },
   watch: {
+    //METODO QUE SE ACCIONA CUANDO LA VARIABLE 'FRACTION' BINDEADA AL INPUT CAMBIA SU VALOR
     fraction: function () {
+      //al recibir la variable 'fraction', se comprueba que tenga el formato de una fraccion
       let a = /^[0-9]{1,}\/[0-9]{1,}$/.test(this.fraction);
       if (a == false) {
+        //si no es una fraccion simplemente muestra un mensaje
         this.fractionresult = "not a fraction";
       } else {
+        //en el caso de ser una fraccion, se envian tanto el numerador como el denominador al metodo 'gcd' escrito mas arriba (linea 42)
         let arr = this.fraction.split("/", 2);
         let div = this.gcd(arr[0], arr[1]);
-
+        //se simplifica la fraccion dividiendo ambas partes de la fraccion por el valor del metodo 'gcd' y se asigna a la variable 'fractionresult' para mostrarlo
         this.fractionresult = arr[0] / div + "/" + arr[1] / div;
       }
     },
+    //METODO QUE SE ACCIONA CUANDO LA VARIABLE 'NAME' BINDEADA AL INPUT CAMBIA SU VALOR
     name: function () {
+      //se testea el string ingresado comparandolo con el regex definido para el caso
       let a = /^([A-Z][a-z]{2,}( [A-Z][a-z]{2,}( [A-Z][a-z]{2,})?| [A-Z]\.( [A-Z][a-z]{2,})?)|([A-Z]\.( [A-Z][a-z]{2,}))|[A-Z]\.( [A-Z]\.)( [A-Z][a-z]{2,}))$/.test(
         this.name
       );
+      //se asigna el valor booleano resultante del test anterior a la variable 'nameresult'
       this.nameresult = a;
     },
   },
